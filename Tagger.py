b743c8630feca3a6d25dce354eb5bcd7bb63cf41
Mehmet <96449723+Mehmet-rg@users.noreply.github.com>
2022-02-10 20:48:34 +0300

    Update Tagger.py

--- Tagger.py
+++ Tagger.py
@@ -165,10 +165,10 @@ async def mentionall(event):
   
   if mode == "text_on_cmd":
     anlik_calisan.append(event.chat_id)
-    usrnum = 1
+    usrnum = 0
     usrtxt = ""
     async for usr in client.iter_participants(event.chat_id):
-      usrnum += 0
+      usrnum += 1
       usrtxt += f"● **[{usr.first_name}](tg://user?id={usr.id})** \n"
       if event.chat_id not in anlik_calisan:
         await event.respond("**𝖤𝗍𝗂𝗄𝖾𝗍𝗅𝖾𝗆𝖾 𝗂𝗌𝗅𝖾𝗆𝗂 𝖻𝖺𝗌𝖺𝗋𝗂𝗒𝗅𝖺 𝖽𝗎𝗋𝖽𝗎𝗋𝗎𝗅𝖽𝗎 ...** \n\n**𝖣𝗂𝗅𝖾𝗋𝗌𝖾𝗇𝗂𝗓 𝖻𝗎𝗋𝖺𝖽𝖺 𝗌𝗂𝗓𝗂𝗇 𝗋𝖾𝗄𝗅𝖺𝗆𝗂𝗇𝗂𝗓 𝗈𝗅𝖺𝖻𝗂𝗅𝗂𝗋 ... \n => [ @StarBotDestek ] <= **")
@@ -183,10 +183,10 @@ async def mentionall(event):
   if mode == "text_on_reply":
     anlik_calisan.append(event.chat_id)
  
-    usrnum = 1
+    usrnum = 0
     usrtxt = ""
     async for usr in client.iter_participants(event.chat_id):
-      usrnum += 0
+      usrnum += 1
       usrtxt += f"● **[{usr.first_name}](tg://user?id={usr.id})** \n"
       if event.chat_id not in anlik_calisan:
         await event.respond("**𝖤𝗍𝗂𝗄𝖾𝗍𝗅𝖾𝗆𝖾 𝗂𝗌𝗅𝖾𝗆𝗂 𝖻𝖺𝗌𝖺𝗋𝗂𝗒𝗅𝖺 𝖽𝗎𝗋𝖽𝗎𝗋𝗎𝗅𝖽𝗎 ...**")
@@ -234,7 +234,7 @@ async def mentionall(event):
     usrtxt = ""
     async for usr in client.iter_participants(event.chat_id):
       usrnum += 1
-      usrtxt += f"**● [{usr.first_name}](tg://user?id={usr.id}) \n"
+      usrtxt += f"● [{usr.first_name}](tg://user?id={usr.id}) \n"
       if event.chat_id not in tekli_calisan:
         await event.respond("**𝖤𝗍𝗂𝗄𝖾𝗍𝗅𝖾𝗆𝖾 𝗂𝗌𝗅𝖾𝗆𝗂 𝖻𝖺𝗌𝖺𝗋𝗂𝗒𝗅𝖺 𝖽𝗎𝗋𝖽𝗎𝗋𝗎𝗅𝖽𝗎 ...** \n\n**𝖣𝗂𝗅𝖾𝗋𝗌𝖾𝗇𝗂𝗓 𝖻𝗎𝗋𝖺𝖽𝖺 𝗌𝗂𝗓𝗂𝗇 𝗋𝖾𝗄𝗅𝖺𝗆𝗂𝗇𝗂𝗓 𝗈𝗅𝖺𝖻𝗂𝗅𝗂𝗋 ... \n => [ @StarBotDestek ] <= **")
         return

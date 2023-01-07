edf59f70d31dedba4f86566520489b91abdd5138
Suphiozturk8 <suphiozturk8@gmail.com>
2023-01-08 01:50:13 +0300

    Update Mention.py

--- Mention.py
+++ Mention.py
@@ -12,7 +12,7 @@ LOGGER = logging.getLogger(__name__)
 
 api_id = int(os.environ.get("9858555"))
 api_hash = os.environ.get("9fe113eb66e7941b5823db5f6e44fcc")
-bot_token = os.environ.get("5704165754:AAFVZvF-0f4HdHf3mxXq3Pn2YakE2fTGBKE")
+bot_token = os.environ.get("5280280454:AAERdvFiRolroIQYNzCSdHeX28n1HK9Vrtk")
 client = TelegramClient('client', api_id, api_hash).start(bot_token=bot_token)
 
 emoji_calisan = []
